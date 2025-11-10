218cadb43aed01b7f340b07393e4548fda4b88be
olivier-portal <olivierportal09@gmail.com>
2025-11-10 15:52:24 +0100

    feat: add ball

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
@@ -9,7 +10,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "[INFO] Demarrage programme...\n";
+    std::cout << "[INFO] Starting programm...\n";
   
     sf::sleep(std::chrono::milliseconds(300));
 
@@ -17,40 +18,71 @@ int main() {
     unsigned int screenHeight = 600;
 
     sf::RenderWindow window;
-    window.create(sf::VideoMode(sf::Vector2u{screenWidth, screenHeight}), "Casse-brique", sf::Style::Default);
+    window.create(sf::VideoMode(sf::Vector2u{screenWidth, screenHeight}), "Bricks Breaker", sf::Style::Default);
 
     if (!window.isOpen()) {
-        std::cerr << "[ERREUR] La fenetre n'a pas pu s'ouvrir.\n";
+        std::cerr << "[ERROR] The window could not open.\n";
         sf::sleep(std::chrono::seconds(3));
         return 1;
     }
 
-    // Visible au premier plan
+    // Show on top and focus
     window.setVisible(true);
     window.requestFocus();
 
-    std::cout << "[INFO] Fenetre ouverte.\n";
+    std::cout << "[INFO] Window open.\n";
 
+    // Set to 60 FPS max
     window.setFramerateLimit(60);
 
-    sf::RectangleShape raquette(sf::Vector2f{100.f, 20.f});
-    raquette.setFillColor(sf::Color(255, 165, 0));
-    raquette.setPosition(sf::Vector2f{100.f, screenHeight - (10.f + raquette.getSize().y)});
+    // Draw paddle & set start position
+    sf::RectangleShape paddle(sf::Vector2f{100.f, 20.f});
+    paddle.setFillColor(sf::Color(255, 165, 0));
+    paddle.setPosition(sf::Vector2f{(screenWidth / 2) - (paddle.getSize().x / 2), screenHeight - (10.f + paddle.getSize().y)});
+
+    // Draw ball & set start position
+    sf::CircleShape ball(10.f);
+    ball.setFillColor(sf::Color::White);
+    ball.setPosition(sf::Vector2f{(screenWidth / 2) - ball.getRadius(), paddle.getPosition().y - (10.f + ball.getRadius() * 2)});
+
+    // Put window boundaries
+    float leftBoundary = 0.f;
+    float rightBoundary = static_cast<float>(screenWidth);
+    float rightPaddleBoundary = static_cast<float>(screenWidth) - paddle.getSize().x;
+
+    // Set boundaries for paddle movement
+    sf::FloatRect boundyBox = paddle.getGlobalBounds();
+
+    float paddleX = paddle.getPosition().x;
+
 
     while (window.isOpen()) {
         while (auto e = window.pollEvent()) {
             if (e->is<sf::Event::Closed>()) window.close();
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-            raquette.move({-1.f, 0.f});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-            raquette.move({1.f, 0.f});
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+            paddle.move({-10.f, 0.f});
+            ball.move({-10.f, 0.f});
+        }
+            
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+            paddle.move({10.f, 0.f});
+            ball.move({10.f, 0.f});
+        }
+
+            if (paddleX < leftBoundary) {
+        paddle.setPosition(sf::Vector2f{leftBoundary, paddle.getPosition().y});
+            } else if (paddleX > rightPaddleBoundary) {
+                paddle.setPosition(sf::Vector2f{rightPaddleBoundary, paddle.getPosition().y});
+            }
+            
         window.clear(sf::Color(57,61,71));
-        window.draw(raquette);
+        window.draw(paddle);
+        window.draw(ball);
         window.display();
     }
 
-    std::cout << "[INFO] Fenetre fermee.\n";
+    std::cout << "[INFO] window closed.\n";
     return 0;
 
 }
